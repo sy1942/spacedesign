fc31d34b9610d556b2c0a609a7f1a1a0ab02a0cd
sy1942 <xymnl@naver.com>
2024-04-26 14:12:44 +0900

    상단헤더 영역 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          more React Story
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <div className="headerWrapper">
+          <div className="logo">
+            <span className="title">공간디자인그룹</span>
+            <span>call 031-546-1999</span>
+          </div>
+          <nav className="gnb">
+            <ul>
+              <li>회사소개</li>
+              <li>제품소개</li>
+              <li>시공사례</li>
+              <li>온라인상담</li>
+            </ul>
+            http://shanzi1579.godhosting.net/bbs/content.php?co_id=company
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
